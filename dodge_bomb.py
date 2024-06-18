2409aaf4667c8d42667eb2a3eb3154c36fa95824
eisho <c0a23096ce@edu.teu.ac.jp>
2024-06-18 14:54:03 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -40,6 +40,9 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        if kk_rct.colliderect(bomb_rct):  # 衝突判定
+            return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, v in keydict.items():
