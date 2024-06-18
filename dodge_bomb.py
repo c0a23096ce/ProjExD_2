55ef7ca2ddd9ed3b00f35a8dc3598d124586b5a6
eisho <c0a23096ce@edu.teu.ac.jp>
2024-06-18 17:26:44 +0900

    課題2途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,12 +9,25 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def inscreen(rect: pg.Rect):
     x, y = True, True
+    # print(f"rect:{type(rect)}")
     if 0 > rect.left or WIDTH < rect.right:
         x = False
     if 0 > rect.top or HEIGHT < rect.bottom:
         y = False
     return x, y
 
+# 課題2
+def circle_accs(r): #半径
+    doublelist = []
+    imglist = []
+    accs = [a for a in range(1, 11)]
+    for i in range(1, 11):
+        img = pg.Surface((r*2*i, r*2*i))
+        pg.draw.circle(img, (255, 0, 0), (r*i, r*i), r*i)
+        img.set_colorkey((0, 0, 0))
+        imglist.append(img)
+        # doublelist.append(pg.draw.circle(img, (255, 0, 0), (r*i, r*i), r*i))
+    return (accs, imglist, doublelist)
 
 # 課題3
 def gameover(screen):
@@ -38,10 +51,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bomb = pg.Surface((20, 20)) # 表示領域
-    bomb.set_colorkey((0, 0, 0)) # 背景透過
-    bomb_rct = pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10) #円の描画
+    accs, bomb_img, surface = circle_accs(10)
+    bomb = bomb_img[0]
+    # bomb = pg.Surface((20, 20)) # 表示領域
+    # bomb.set_colorkey((0, 0, 0)) # 背景透過
+    # bomb_rct = pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10) # = 
+    bomb_rct = bomb.get_rect()
     bomb_rct.center = randint(0, WIDTH), randint(0, HEIGHT)
+
+
     clock = pg.time.Clock()
     tmr = 0
     keydict = {
@@ -62,8 +80,9 @@ def main():
         (0, -5):270,
         (5, -5):315
         }
-    
-
+    # 課題2
+    # accs, bomb_size, surface = circle_accs(10)
+    # accs, bomb_img, surface = circle_accs(10)
 
     vx, vy = +5, +5
     while True:
@@ -73,10 +92,10 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         if kk_rct.colliderect(bomb_rct):  # 衝突判定
-
+            pass
             #課題3
-            gameover(screen)
-            return
+            # gameover(screen)
+            # return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -85,15 +104,29 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+
         if inscreen(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bomb_rct.move_ip(vx, vy)
+        
+        #bomb_rct = surface[min(tmr//500, 9)]
+        #print(f"bomb_rct:{bomb_rct}")
+        # print(f"surface:{bomb_img}")
+
+
         x, y = inscreen(bomb_rct)
         if not x:
             vx *= -1
         if not y:
             vy *= -1
+        bomb_rct.move_ip(vx*accs[min(tmr//500, 9)], vy*accs[min(tmr//500, 9)])
+
+        bomb = bomb_img[min(tmr//500, 9)]
+        bomb_rct = bomb.get_rect()
+
+        print(bomb_rct.center,bomb_rct,bomb_rct.width, bomb_rct.height)
+
+        
         screen.blit(bomb, bomb_rct)
         pg.display.update()
         tmr += 1
